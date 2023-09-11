d6375ce369833c953bab7f4ffac219ee7a44d498
pufferbommy <pufferbommy@gmail.com>
2023-09-11 23:55:17 +0700

    chore: test auth functions

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private router = inject(Router);
+
   login(email: string, password: string) {
     localStorage.setItem('userId', '1');
+    this.router.navigate(['/admin']);
   }
 
   logout() {
     localStorage.removeItem('userId');
+    this.router.navigate(['/auth']);
   }
 
   isLoggedIn() {
--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent {}
+export class AdminComponent {
+  private authService = inject(AuthService);
+
+  logout() {
+    this.authService.logout();
+  }
+}
