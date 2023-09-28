4fe10f7a3f65a3a2fc46cf44faaa7066c69e5eeb
Toon <wisitmoondet@gmail.com>
2023-09-28 12:36:06 +0700

    chore : logout user

--- src/app/features/user/pages/main/main.component.ts
+++ src/app/features/user/pages/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-main',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
   selectedNavItem: string = '';
+
+  private authService = inject(AuthService);
+
+  logout() {
+    this.authService.logout();
+  }
 }
